8486024cd938258f582066992cedf647636e6a10
sreerajchintham <61490555+sreerajchintham@users.noreply.github.com>
2023-05-01 23:39:44 -0400

    Update test.sh

--- main folders/test.sh
+++ main folders/test.sh
@@ -7,4 +7,4 @@ trap 'kill $PID' EXIT # kill the server on exit
 PID=$! # record the PID
 
 newman run Basic Test.postman_collection.json -e env.json # use the env file
-newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
+newman run Basic Test.postman_collection.json -n 50 # 50 iterations
